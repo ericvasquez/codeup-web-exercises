ea1e9e65b6f0de13ce7a24b36c4b9d877eea30b9
ericvasquez <eric.vasquez3327@gmail.com>
2018-12-19 13:31:20 -0600

    corrected function to return last commit date

---
+++ js/promises.js
@@ -0,0 +1,36 @@
+
+function wait(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+
+
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+
+const API_TOKEN = 'e0100948d722658bd993f764e5bc018422fe26b1';
+
+const lastCommit = {
+    headers: {'Authorization': `token ${API_TOKEN}`}
+};
+
+fetch('https://api.github.com/repos/ericvasquez/codeup-web-exercises/commits', lastCommit)
+    .then(function (response) {
+        if(response.status !== 200){
+            return Promise.reject(response);
+        }
+        return Promise.resolve(response);
+    })
+
+.then(function (response) {
+    const jsonResponse = response.json();
+    return jsonResponse;
+})
+    .then(function (data) {
+
+        console.log("The last commit was made: " + data[0].commit.author.date);
+    });
